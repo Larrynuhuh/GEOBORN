dccc32a3b377c68e4c3cc83727dff4181212f860
Larrynuhuh <swastiksingh1402@gmail.com>
2026-02-16 21:46:11 +0530

    midpoint function

--- ops/metrics.py
+++ ops/metrics.py
@@ -1,8 +1,15 @@
 @jax.jit
-def line_len(l):
+def linlen(l):
     
     diff = jnp.diff(l, axis = 0)
     lens = jnp.linalg.norm(diff, axis = 1)
     sums = jnp.sum(lens)
 
     return sums
+
+def midp(p1, p2):
+    mid = (p1 + p2) / 2.0
+    return mid
+
+vmidp = jax.vmap(midp)
+
